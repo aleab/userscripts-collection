c6b34dec2509a1c3bcce78e874764588780066db
Alessandro Attard Barbini <11708634+aleab@users.noreply.github.com>
2019-08-28 23:32:28 +0200

    [tennoware.com] Organize /mybuilds page by type

--- warframe/tennoware.com.js
+++ warframe/tennoware.com.js
@@ -9,10 +9,14 @@
         return map.get(obj);
     };
 
-    function Tennoware() { }
+    function Tennoware() {
+        _(this).weaponTypeToNameMap = {};
+    }
 
     Tennoware.prototype.doStuff = async function() {
         console.log(`WARFRAME | tennoware.com: ${window.location.href}`);
+
+        await populateWeaponTypeToNameMap(this);
 
         let currentUrl = window.location.href;
         if (REGEX_MYBUILDS_PAGE.test(currentUrl)) {
@@ -30,6 +34,18 @@
     //  PRIVATE FUNCTIONS
     // ===================
 
+    const populateWeaponTypeToNameMap = async function(self) {
+        await waitFor('.topbar .nav-menu > .nav-options');
+
+        _(self).weaponTypeToNameMap = {};
+        let navOptions = $('.topbar .nav-menu > .nav-options a.nav-option');
+
+        for (let i = 0; i < navOptions.length; ++i) {
+            let type = navOptions[i].href.replace(/^.*\/(.+)$/, '$1');
+            _(self).weaponTypeToNameMap[type] = $(navOptions[i]).text();
+        }
+    }
+
     const mybuilds = {
         prettifyMyBuildsPageLayout: async function(self) {
             await waitFor('.my-builds > .builds-wrapper > .my-build-container');
@@ -37,20 +53,47 @@
 
             // Sort by item name
             builds.sort((a, b) => {
-                let at = $(a).find('.my-item-name').text();
-                let bt = $(b).find('.my-item-name').text();
-                if (at > bt) return 1;
-                if (at < bt) return -1;
-
-                at = $(a).find('.my-build-list-name').text();
-                bt = $(b).find('.my-build-list-name').text();
-                if (at > bt) return 1;
-                if (at < bt) return -1;
-                return 0;
+                let result = $(a).find('.my-item-name').text().localeCompare($(b).find('.my-item-name').text());
+                if (result === 0)
+                    result = $(a).find('.my-build-list-name').text().localeCompare($(b).find('.my-build-list-name').text());
+                return result;
             });
+            $('.my-builds > .builds-wrapper').append(builds.remove());
 
-            $('.my-builds > .builds-wrapper > .my-build-container').remove()
-            $('.my-builds > .builds-wrapper').append(builds);
+            // Organize by item type
+            let buckets = {};
+            for (let i = 0; i < builds.length; ++i) {
+                let href = $(builds[i]).find('.my-build-buttons > a:has(div.interactable:has(p:contains("Open")))')[0].href;
+                let type = REGEX_BUILD_PAGE.exec(href)[1];
+                (buckets[type] ? buckets[type] : (buckets[type] = [])).push(i);
+            }
+
+            let wrapper = $(document.createElement('div')).addClass('flex-wrapper-c').insertAfter($('.my-builds > .builds-wrapper > .my-builds-subtitle')[0]);
+            for (let type in _(self).weaponTypeToNameMap) {
+                if (!buckets[type]) continue;
+                let name = _(self).weaponTypeToNameMap[type];
+
+                $(document.createElement('div')).addClass(['collapsible-group', 'flex-wrapper-c']).append(
+                    $(document.createElement('button')).addClass('collapsible').click(ev => {
+                        let group = $(ev.currentTarget).closest('.collapsible-group');
+                        group.find('.collapsible-content').toggleClass('hidden');
+                        group.toggleClass('active');
+                        $(ev.currentTarget).find('i.fas.fa-chevron-right').toggleClass('rot90');
+                    }).append(
+                        $(document.createElement('i')).addClass(['fas', 'fa-chevron-right'])
+                    ).append(
+                        $(document.createElement('span')).append(name)
+                    )
+                ).append(
+                    $(document.createElement('div')).addClass('collapsible-content').addClass('hidden').append(
+                        buckets[type].map(i => builds[i])
+                    )
+                ).appendTo(wrapper);
+            }
+
+            // Remove the search field, since it's broken now that the builds have been moved
+            $('.my-builds > .my-builds-topbar').remove();
+            $('.my-builds > .builds-wrapper').css({ 'padding-top': '10px' });
         }
     };
 
@@ -123,6 +166,66 @@
     miscTweaks = async function(self) {
         await waitFor('.topbar');
         $('.topbar > .top-buttons > a[href$="/mybuilds"]').attr('href', 'javascript:;')
+
+        // Custom Styles
+        let S = function() {
+            let str = '';
+            for (let i = 0; i < arguments.length; ++i)
+                str += (arguments[i] ? arguments[i] : '') + '\n';
+            return str + '\n';
+        };
+        let R = function(s, n) {
+            let str = '';
+            for (let i = 0; i < n; ++i)
+                str += s;
+            return str;
+        }
+
+        $('#fontawesome-styles').remove();
+        $(document.createElement('link')).attr('id', 'fontawaesome-styles').attr('rel', 'stylesheet').attr('type', 'text/css').attr('href', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/fontawesome.min.css').appendTo(document.head);
+
+        $('#fontawesome-styles-solid').remove();
+        $(document.createElement('link')).attr('id', 'fontawaesome-styles-solid').attr('rel', 'stylesheet').attr('type', 'text/css').attr('href', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/solid.min.css').appendTo(document.head);
+
+        $('#aleab-styles').remove();
+        $(document.createElement('style')).attr('id', 'aleab-styles').attr('type', 'text/css')
+            .append(S(
+                '.hidden { display: none !important; }',
+                '.flex-wrapper-c { display: flex; flex-direction: column; align-items: center; }',
+                '.rot90 { transform: rotate(90deg); }'
+            ))
+            .append(S(
+                '.collapsible-group {',
+                '    width: 97vw;',
+                '    max-width: 820px;',
+                '    margin-bottom: 4px;',
+                '}',
+                '.collapsible-group.active { margin-bottom: 12px; }'
+            ))
+            .append(S(
+                '.collapsible-group > .collapsible {',
+                '    align-self: flex-start;',
+                '    background-color: transparent;',
+                '    border: none;',
+                '    outline: none;',
+                '    cursor: pointer;',
+                '    padding: 0px 0px 0px 4px;',
+                '    color: #F0D975;',
+                '    font-size: 17px;',
+                `    text-shadow: black 0px 0px 2px${R(', black 0px 0px 2px', 7)};`,
+                '}',
+                '.collapsible-group > .collapsible > .fas { font-size: 14px; }',
+                '.collapsible-group > .collapsible > span { padding-left: 4px; }',
+                '.collapsible-group.active > .collapsible > span { padding-left: 6px; }'
+            ))
+            .append(S(
+                '.collapsible-group > .collapsible-content {',
+                '    display: block;',
+                '    overflow: hidden;',
+                '    max-width: 790px;',
+                '}'
+            ))
+            .appendTo(document.head);
     }
 
     return Tennoware;
